9c190dbe576f49ff28721c336abfd39a121f59b7
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2024-11-19 16:16:28 -0300

    list dataflows as table

--- power-query/dataflows.m
+++ power-query/dataflows.m
@@ -6,15 +6,17 @@ let
         {
             "Data",
             each
-               [
-                   Dataflow = [dataflowName]{0},
-                   Data = [Data]{0}
-               ]
+               List.Zip({[dataflowName], [Data]})
         }
     ),
-    #"Dataflow Expandido" = Table.ExpandRecordColumn(Dataflows, "Data", {"Dataflow", "Data"}, {"Dataflow", "Data"}),
-    #"Entity Expandido" = Table.ExpandTableColumn(#"Dataflow Expandido", "Data", {"entity"}, {"Tables"}),
-    #"Outras Colunas Removidas" = Table.SelectColumns(#"Entity Expandido",{"workspaceName", "Dataflow", "Tables"}),
+    #"Data Expandido" = Table.ExpandListColumn(Dataflows, "Data"),
+    #"List to Record" = Table.TransformColumns(
+        #"Data Expandido",
+        {"Data", each [ Dataflow = _{0}, Table = _{1}]}
+    ),
+    #"Record Expandido" = Table.ExpandRecordColumn(#"List to Record", "Data", {"Dataflow", "Table"}),
+    #"Table Expandido" = Table.ExpandTableColumn(#"Record Expandido", "Table", {"entity"}, {"Tables"}),
+    #"Outras Colunas Removidas" = Table.SelectColumns(#"Table Expandido",{"workspaceName", "Dataflow", "Tables"}),
     #"Tipo Alterado" = Table.TransformColumnTypes(#"Outras Colunas Removidas",{{"workspaceName", type text}, {"Dataflow", type text}, {"Tables", type text}})
 in
     #"Tipo Alterado"
