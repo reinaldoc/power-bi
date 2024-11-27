5ca8c0b9fc5b421711466298f9d5682b83ba2502
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2024-11-27 18:49:30 -0300

    create pbip files script added

---
+++ scripts/create-pbip-files.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ls -d *.Report | while read report; do
+  name=${report/.Report/}
+  test -f "${name}.pbip" && continue
+  cat <<EOF > "$name.pbip"
+{
+  "version": "1.0",
+  "artifacts": [
+    {
+      "report": {
+        "path": "$name.Report"
+      }
+    }
+  ],
+  "settings": {
+    "enableAutoRecovery": true
+  }
+}
+EOF
+
+done
