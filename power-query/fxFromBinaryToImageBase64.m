ea5ed285151ead4bb901ab6e0b508b4156998dcd
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2025-07-18 13:43:40 -0300

    m function to convert binary image to html base64 string

--- power-query/fxFromBinaryToImageBase64.m
+++ power-query/fxFromBinaryToImageBase64.m
@@ -1,3 +1,19 @@
+/*
+
+  Function to convert binary imagem to HTML base64 string.
+
+  How to use this function using [Content] column as binary image?
+
+  BinaryToBase64 =
+    Table.TransformColumns(
+      PREVIOUS_STEP,
+      {
+        {"Content", fxFromBinaryToImageBase64, type text }
+      }
+    )
+
+*/
+
 (image as binary) as text =>
 let
     Base64 = Binary.ToText(image, BinaryEncoding.Base64),
