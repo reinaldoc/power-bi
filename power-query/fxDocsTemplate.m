b47e18b6015fc83c9a5d3a6830ecb06f54011d18
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2025-07-27 00:46:35 -0300

    m documented function template

--- power-query/fxDocsTemplate.m
+++ power-query/fxDocsTemplate.m
@@ -1,16 +1,19 @@
 /*
-    Exemplo de função documentada.
+    Exemplo de função fxSomaValores() documentada.
     Essas informações são apresentadas ao clicar na função.
 */
 
 Value.ReplaceType(
+    // código da função fxSomaValores()
     (valor1, valor2) =>
         let
             Etapa1 = valor1 + valor2
         in
             Etapa1,
+
+    // tipos dos parâmetros, informações e exemplos de uso da função
     type function (valor1 as number, valor2 as number) as number meta [
-        Documentation.Name = "SomaValores",
+        Documentation.Name = "fxSomaValores",
         Documentation.LongDescription = "Esta função retorna a soma de dois números.",
         Documentation.Description = "Soma dois valores",
         Documentation.Examples = {
