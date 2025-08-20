dfe0578bcaa3241f9c35dc2a0850db697659c93a
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2025-08-20 00:58:19 -0300

    Table.TransformRows + Record.TransformFields example to transform many columns

---
+++ power-query/transformRows.m
@@ -0,0 +1,25 @@
+let
+    Fonte =
+        #table(
+            {"Pedido", "Moeda", "Valor"},
+            {
+                {1, "BRL", 300},
+                {2, "BRL", 400},
+                {3, "USD", 100}
+            }
+        ),
+    Resultado =
+        Table.FromRecords(
+            Table.TransformRows(
+                Fonte,
+                each Record.TransformFields(
+                    _,
+                    { 
+                        { "Pedido", (p) => "P" & Text.PadStart(Text.From(p), 5, "0") },
+                        { "Valor",  (v) => if [Moeda] = "USD" then v * 5 else v }
+                    }
+                )
+            )
+        )
+in
+    Resultado
