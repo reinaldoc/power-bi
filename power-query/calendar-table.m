7c932e6baeba592242392888998867c6b7f7b444
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2025-09-03 15:24:22 -0300

    calendar table updated to identify current year/month

--- power-query/calendar-table.m
+++ power-query/calendar-table.m
@@ -1,6 +1,12 @@
 let
-    // Configure a coluna de datas da Tabela Fato
-    Coluna_data  = List.Buffer(F_Vendas[Data]),
+    Hoje = Date.From(DateTimeZone.UtcNow() - #duration(0, 3, 0, 0)),
+    // Ou defina as datas iniciais e finais
+    Coluna_data  = {
+        #date(2025, 1, 1),   // data com ano inicial
+        Hoje                 // data com ano final
+    },
+    // Ou indique a coluna de datas da Tabela Fato
+    // Coluna_data  = List.Buffer(F_Vendas[Data]),
     Data_minima  = List.Min(Coluna_data),
     Data_maxima  = List.Max(Coluna_data),
     Data_inicial = Date.StartOfYear(Data_minima),
@@ -18,6 +24,7 @@ let
                 Dia da semana = Int64.Type,
                 Dia da semana por extenso = text,
                 #"Mês/Ano" = text,
+                #"Mês/Ano atual" = text,
                 #"Mês/Ano ordenada" = Int64.Type,
                 Trimestre = text,
                 Semestre = text,
@@ -38,6 +45,14 @@ let
                         "/",
                         Text.From(Date.Year(_))
                     }),
+                    if Date.Year(Hoje) = Date.Year(_) and Date.Month(Hoje) = Date.Month(_) then
+                        "Mês atual"
+                    else
+                        Text.Combine({
+                            Text.Start(Date.MonthName(_), 3),
+                            "/",
+                            Text.From(Date.Year(_))
+                        }),
                     (Date.Year(_) - Date.Year(Data_inicial)) * 12 + Date.Month(_),
                     Text.From(Date.QuarterOfYear(_)) & "º Trim.",
                     if Date.Month(_) <= 6 then "1º Sem." else "2º Sem.",
