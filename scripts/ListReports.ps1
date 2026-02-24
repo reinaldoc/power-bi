8cdd741933dc78f3b2035fb83e6f59c4e5665f7c
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2026-02-24 15:01:26 -0300

    PowerShell scripts to list semantic models and reports

--- scripts/ListReports.ps1
+++ scripts/ListReports.ps1
@@ -5,7 +5,7 @@ Login-PowerBI
 # $WorkspaceId = "00000000-1111-2222-3333-444444444444"
 $WorkspaceId = Read-Host -Prompt "Enter Workspace ID: "
 
-$semanticmodels = Get-PowerBIDataset -WorkspaceId $WorkspaceId
+$semanticmodels = Get-PowerBIReport -WorkspaceId $WorkspaceId
 
 foreach($sm in $semanticmodels.GetEnumerator()) {
     $id = $($sm.Id)
